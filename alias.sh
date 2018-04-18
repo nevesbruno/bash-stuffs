09f108fac8a8e3aab13b21f6bc29e70d76601d26
Bruno Neves <bruno.neves@justdigital.com.br>
2018-04-18 16:23:19 -0300

    Add git branch alias avoiding confilict with branch apt-get package

--- alias.sh
+++ alias.sh
@@ -19,6 +19,20 @@ alias runserver="command python -m SimpleHTTPServer"
 alias html5="command git clone https://github.com/h5bp/html5-boilerplate.git"
 alias bashedit="vi ~/.zshrc"
 alias a="vim ~/lab/bash-stuffs/alias.sh"
+alias hosts="sudo vim /etc/hosts"
+alias chosts="cat /etc/hosts"
+
+## NPM
+function clear-npm(){
+    echo "Cleaning package-lock"
+    rm ./package-lock.json
+    echo "Cleaning node_modules"
+    rm -r ./node_modules
+    echo "Cleaning npm cache ~with the Force"
+    npm cache clear --force
+    echo "Reinstalling modules"
+    npm i
+}
 
 ## Sublime
 function s(){
@@ -41,6 +55,7 @@ function vimedit(){
 
 ### Aliases
 alias gst="git status"
+alias gbranch="git branch"
 alias stash="git stash"
 alias pop="git stash pop"
 alias glog="git log --graph --all --pretty=format:'%C(yellow)%h %ad %Cblue%an%C(auto)%d %Creset%s' --date=short --decorate"
@@ -70,6 +85,16 @@ function push(){
 # Pull from a branch
 function pull(){
     git pull $1 $2
+}
+
+# Checkout
+function checkout(){
+    git checkout $1 $2
+}
+
+# Merge
+function merge(){
+    git merge $1
 }
 
 # Add all non commited files and set a commit message
